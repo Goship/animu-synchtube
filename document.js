8ce24c7c8c4081f0e119c17ad8eb00ad221c5ef0
Falaina <rudd@csail.mit.edu>
2011-07-07 16:38:35 -0400

    banner html

---
+++ document.js
@@ -0,0 +1,60 @@
+var html = 
+'<div style="visibility: hidden">Welcome to /a/\'s synchtube, enjoy your stay :3                                                                                          </div>\
+\
+<p><img src="//nodocchi.com/random" style="border: 1px solid #ccc;margin-left:auto; margin-right:auto; display:block;" width="100%" /></p>\
+<p>\
+\
+<br />\
+\
+<p>\
+	<link href="//nodocchi.com/nodocch" rel="stylesheet" />\
+</p>\
+<div id="ie-test">\
+	<ul class="group" id="boxLinks">\
+		<li>\
+			<a href="#box1"><b>Main</b></a></li>\
+		<li>\
+			<a href="#box2"><b>Etc</b></a></li>\
+	</ul>\
+	<div id="box">\
+		<div class="box" id="box1">\
+			<ul>\
+				<br />\
+				Defeated: ろん -「おちゃめ機能」FUKKIRETA 10 hours &bull; Cosmos: A Personal Voyage &bull; Night of Fire ft. Korikki 10 HOURS LONG &bull; (touhou) Myomyomyomyomyomyomyon! (HD) 3 HOURS LONG &bull; Cromartie Highschool dub &bull; Nyoron Churuya san &bull; Di Gi Charat &bull; The Melancholy of Haruhi-Chan &bull; Battle Programmer Shirase &bull; Hokago no Pleiades &bull; REC &bull; The Disappearance Of Haruhi Suzumiya &bull; Fate/Stay night UBW &bull; Girl who leapt through time &bull; 24 hour tag &bull; Mars of Destruction &bull; Black Rock Shooter OVA &bull; Interstella 5555 &bull; Kikujiro &bull; Kara no Kyoukai &bull; Fist of the North Star Movie Remastered &bull; The Legend of Koizumi &bull; High School of The Dead &bull; Bakemonogatari &bull; Toradora! &bull; Majokko Tsukune-Chan &bull; Funky Forest: The First Contact &bull; Clannad &bull; Seitokai Yakuindomo &bull; Eve no Jikan &bull; .Hack//Quantum &bull; Panty &amp; Stocking with Garterbelt &bull; Dokuro-Chan &bull; Ah! My Goddess: The Movie &bull; Akira &bull; Rozen Maiden S1<br />\
+				<br />\
+				On next anime hour, 10 PM EST: Rozen Maiden: Traumend 5-6<br />\
+				<br />\
+				Animu we are watching: Denpa Onna (to ep 9), Aria (to ep 5), Minami-Ke (to ep 7), Astro Fighter Sunred (Ep 1)<br />\
+				<br />\
+				Submit your own animu banner. 1000x116 size, .png file, with these font :3 http://www.dafont.com/the-great-escape.font\
+			</ul>\
+		</div>\
+		<div class="box" id="box2">\
+			<br />\
+			<b>Etc:</b>\
+			<ul style="list-style-type: square; margin-left:16px">\
+				<li>\
+					DJZebro collected these all for you. It&#39;s not like he wanted to or anything, just take it! <a href="//tinyurl.ru/g2l0">DJZebro_Pack1</a>, <a href="//tinyurl.ru/gknk">DJZebro_Pack2</a>, <a href="//tinyurl.ru/gtc5"> DystopiaGroundPack</a></li>\
+				<li>\
+					Denwa has uploaded some animu on youtube: <a href="//tinyurl.ru/g2mc">Denwa&#39;s Channel</a></li>\
+				<li>\
+					Live vn wiki: livevn.wikia.com</li>\
+				<li>\
+					Polls may or may not be posted automatically to: http://livevn.wikia.com/wiki/Polldump</li>\
+				<li>\
+					http://steamcommunity.com/groups/naitofiyah/</li>\
+				<li>\
+					We play vidya games, somehow. So far, openly playing: League of Legends, Vindictus, Minecraft</li>\
+				<li>\
+					Yea so if a drawfag could get on this that would be sugoi. http://i.imgur.com/72h7D.png</li>\
+			</ul>\
+		</div>\
+	</div>\
+</div>\
+<br />\
+<br />\
+<p>\
+	<marquee bgcolor="#eef2ff" loop="-1" onmouseout="this.start();" onmouseover="this.stop();" scrollamount="1" width="100%"><b>(✖&#39;___&#39;) (&#39;______________________________________________________________________________________________________________________________&#39;✖)</b></marquee></p>\
+'
+
+$(".tv-descr-test").html(html);;
