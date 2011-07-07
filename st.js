925b26fc71ae495286e7831f5b921e585e1f77c1
Falaina <rudd@csail.mit.edu>
2011-07-07 15:58:20 -0400

    Adding word filter

--- st.js
+++ st.js
@@ -1,16 +1,49 @@
-var replaceModvatar = function(mod, url) {
-    $('img.user_id[alt='+mod+']').replaceWith('<img src='+url+' class=user_id alt='+mod+' id='+mod+'>');
-    $('#'+mod).addClass('mod-avatar');
-}
-
-var modvatars = [
+var modvatars = [ //Replacement avatars
     { mod : 'Keii',      url : '//tinyurl.ru/hkei'},
     { mod : 'DJZebro',   url : '//tinyurl.ru/hkmm'},
     { mod : 'Nodocchi',  url : '//nodocchi.com/nodocchi'},
     { mod : 'RAILGUN',   url : '//oi56.tinypic.com/714eqh.jpg'},
     { mod : 'Xiox',      url : '//i.imgur.com/pWCYo.gif'},
     { mod : 'Tofutoshi', url : '//i.imgur.com/pOBRZ.gif'},
-    { mod : 'AnimuXD',   url : '//i.imgur.com/t1YlE.gif'}];    
+    { mod : 'AnimuXD',   url : '//i.imgur.com/t1YlE.gif'}
+];    
+var word_filters = [ // Filtered words
+    {pat : /madoka/ig,       new : 'meduca'},
+    {pat : /homura/ig,       new : 'hameru'},
+    {pat : /mami/ig,         new : 'mumi'},
+    {pat : /kyoko/ig,        new : 'kyaku'},
+    {pat : /sayaka/ig,       new : 'sayaku'},
+    {pat : /(binaryheap)/ig, new : '$1-senpai'}
+];
+
+
+// Convenience function for hooking a javascript function
+var instrumentFn = function(fn, pre) {
+    argTransform = arguments[2];
+    var newFn = function() 
+    {
+	var res = pre.apply(this, arguments); 
+	if(argTransform) {arguments = res;}
+        if(fn.instrumented) {fn = fn.restore();}
+	return fn.apply(this, arguments)
+    };
+    newFn.restore = function() {return fn;}
+    newFn.instrumented = true;
+    return newFn;
+}
+
+var wordFilter = function(usr, msg, wat) {
+    for(p in word_filters) {
+	msg = msg.replace(word_filters[p].pat, word_filters[p].new);
+    }
+    return [usr, msg, wat];
+}
+
+var replaceModvatar = function(mod, url) {
+    $('img.user_id[alt='+mod+']').replaceWith('<img src='+url+' class=user_id alt='+mod+' id='+mod+'>');
+    $('#'+mod).addClass('mod-avatar');
+}
+
 
 var replaceModvatars = function () {
     for(i in modvatars) {
@@ -20,6 +53,7 @@ var replaceModvatars = function () {
 
 var doit = function (){
     replaceModvatars();
+    chat.writeMessage = instrumentFn(chat.writeMessage, wordFilter, true);
     $.getScript('//cloud.github.com/downloads/malsup/cycle/jquery.cycle.all.2.74.js', function () {
         $('.slideshow').cycle({
             fx: 'fade',
